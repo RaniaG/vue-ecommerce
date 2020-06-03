ae9eb8771b7933116f0f430a3f915fc818084dc6
Rania.Gamal <rania.gamal@integrant.com>
2020-06-03 06:58:44 +0200

    user store

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,36 +1,36 @@
 export default {
     state: {
         userId: null,
-        userName: 'rania',
+        userName: '',
         photo: '',
         roles: [],
         token: ''
     },
     getters: {
-        isLoggedIn() {
-            return this.userId != null;
+        isLoggedIn(state) {
+            return state.userId != null;
         },
-        isInRole(role) {
-            return this.isLoggedIn() && this.roles.find(e => e == role);
+        isInRole(state, role) {
+            return state.userId != null && state.roles.find(e => e == role);
         },
-        getToken() {
-            return this.token;
+        getToken(state) {
+            return state.token;
         }
     },
     mutations: {
-        setUser({ userId, userName, photo, roles, token }) {
-            this.userId = userId;
-            this.userName = userName;
-            this.photo = photo;
-            this.roles = roles;
-            this.token = token;
+        setUser(state, { userId, userName, photo, roles, token }) {
+            state.userId = userId;
+            state.userName = userName;
+            state.photo = photo;
+            state.roles = roles;
+            state.token = token;
         },
-        clearUser() {
-            this.userId = null;
-            this.userName = 'rania';
-            this.photo = '';
-            this.roles = [];
-            this.token = '';
+        clearUser(state) {
+            state.userId = null;
+            state.userName = '';
+            state.photo = '';
+            state.roles = [];
+            state.token = '';
         }
     }
 }
--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,3 @@
-
 import Vue from 'vue';
 import Vuex from 'vuex';
 import user from './modules/user';
@@ -6,8 +5,9 @@ import cart from './modules/cart';
 Vue.use(Vuex);
 export const store = new Vuex.Store({
     modules: {
-        user: { ...user, namespaced: true },
-        cart: { ...cart, namespaced: true }
+        user: {...user, namespaced: true },
+        cart: {...cart, namespaced: true }
 
-    }
+    },
+    strict: true
 });
